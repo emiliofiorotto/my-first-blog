8d527b6c21913cd0de6b790da3c8d2ed4b747dad
emiliofiorotto <emiliofiorotto@gmail.com>
2022-11-12 20:45:14 -0300

    Actualización de home, cv y blog

--- blog/models.py
+++ blog/models.py
@@ -1,9 +1,10 @@
 from django.db import models
 import datetime
-
+from django.db.models.fields import CharField, URLField
 
 class Post(models.Model):
     title = models.CharField(max_length=100)
     description = models.TextField()
     image = models.ImageField(upload_to='blog/images')
     date = models.DateField(datetime.date.today)
+    url = URLField(blank=True)
