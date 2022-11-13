bfc17a951de4d5c4bed803d6f119bf9fe21328bf
emiliofiorotto <emiliofiorotto@gmail.com>
2022-11-12 21:34:38 -0300

    edit home por index

--- portfolio/views.py
+++ portfolio/views.py
@@ -7,6 +7,6 @@ from .models import Project
 def home(request):
     projects = Project.objects.all()
 
-    return render(request, 'home.html', {
+    return render(request, 'index.html', {
         'projects': projects
     })
